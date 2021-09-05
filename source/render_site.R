07929708db8bd9e9ccc871aeaa666a7315ddfd43
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-06 02:07:54 +1000

    rebuilds site

--- source/render_site.R
+++ source/render_site.R
@@ -1,6 +1,7 @@
+root <- rprojroot::find_root("_site.yml")
 rmarkdown::render_site()
 fs::file_copy(
-  path = here::here("_redirects"),
-  new_path = here::here("_site", "_redirects"),
+  path = fs::path(root, "_redirects"),
+  new_path = fs::path(root, "_site", "_redirects"),
   overwrite = TRUE
 )
