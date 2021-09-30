c76a3c58199bbd64524c773e27fb327a9736e13f
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-30 18:34:47 +1000

    cleans up blog root folder

--- source/render_site.R
+++
@@ -1,7 +0,0 @@
-root <- rprojroot::find_root("_site.yml")
-rmarkdown::render_site()
-fs::file_copy(
-  path = fs::path(root, "_redirects"),
-  new_path = fs::path(root, "_site", "_redirects"),
-  overwrite = TRUE
-)
--- source/restore_post_envs.R
+++
@@ -1,5 +0,0 @@
-slug <- refinery::articles_matching("welcome")
-refinery::renv_new(slug)
-
-refinery::renv_snapshot(slug)
-refinery::renv_restore(slug)
